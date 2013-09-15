db2be22427641ef0086ef283108f7195e6509745
syqlo <saurabh.talbar@gmail.com>
2013-09-15 12:59:16 -0700

    updated return status for rest api (ItemResource.java)

--- src/main/java/com/service/ItemResource.java
+++ src/main/java/com/service/ItemResource.java
@@ -13,6 +13,8 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 
 import com.model.ItemBean;
 
@@ -24,38 +26,59 @@ public class ItemResource {
 
 	@GET
 	@Path("{title}")
-	public ItemBean getItem(@PathParam("title") String title) {
+	public Response getItem(@PathParam("title") String title) {
+
+		if(title==null || title.trim().length()==0)
+			return Response.serverError().entity("title cannot be blank").build();		
+
 		if(App.mongoInstance.contains(title))
-			return App.mongoInstance.get(title);
+			//return App.mongoInstance.get(title);
+			return Response.ok(App.mongoInstance.get(title), "application/json").build();
 		else
-			return new ItemBean("null","null",false);
+			throw new WebApplicationException(404);
 	}
 
 	@POST
-	public ItemBean createItem(ItemBean input) {		
+	public Response createItem(ItemBean input) {		
+
+		if(input==null)
+			return Response.serverError().entity("json input cannot be blank").build();
 
 		//insert/update item into mongodb
-		if(App.mongoInstance.contains(input.getTitle()))
+		if(App.mongoInstance.contains(input.getTitle())){
 			App.mongoInstance.update(input);
-		else
+			return Response.status(200).entity("Item with title "+input.getTitle()+" updated successfully").build();
+		}
+		else{
 			App.mongoInstance.put(input);
-		return input;
+			return Response.status(200).entity("Item with title "+input.getTitle()+" created successfully").build();
+		}
 	}
-	
+
 	@PUT
-	public ItemBean updateItem(ItemBean input) {		
+	public Response updateItem(ItemBean input) {		
+
+		if(input==null)
+			return Response.serverError().entity("json input cannot be blank").build();
 
 		//insert/update item into mongodb
-		if(App.mongoInstance.contains(input.getTitle()))
+		if(App.mongoInstance.contains(input.getTitle())){
 			App.mongoInstance.update(input);
-		else
+			return Response.status(200).entity("Item with title "+input.getTitle()+" updated successfully").build();
+		}
+		else{
 			App.mongoInstance.put(input);
-		return input;
+			return Response.status(200).entity("Item with title "+input.getTitle()+" created successfully").build();
+		}
 	}
+
 	@DELETE
 	@Path("{title}")
-	public void deleteItem(@PathParam("title") String title){
-		App.mongoInstance.delete(title);
+	public Response deleteItem(@PathParam("title") String title){
+		if(title==null || title.trim().length()==0)
+			return Response.serverError().entity("title cannot be blank").build();		
 
+		App.mongoInstance.delete(title);
+		return Response.status(200).entity("Item with title "+title+" deleted successfully").build();
 	}
 }
