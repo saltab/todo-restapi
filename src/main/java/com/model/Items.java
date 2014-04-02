3534ce04f23ec50023baefc1296221e71b7bf2f7
syqlo <saurabh.talbar@gmail.com>
2014-04-02 00:07:52 -0700

    Adding a new model for supporting list of ItemBean

---
+++ src/main/java/com/model/Items.java
@@ -0,0 +1,25 @@
+package com.model;
+
+/**
+ * @author Saurabh Talbar (saurabh.talbar at gmail.com)
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+
+// Class used to save list of ItemBeans
+public class Items {
+	List<ItemBean> items;// = new ArrayList<ItemBean>();
+	
+	public Items(List<ItemBean> list){
+		this.items = new ArrayList<ItemBean>(list);
+	}
+	
+	public List<ItemBean> getItems(){
+		return this.items;
+	}
+	
+	public void setItems(List<ItemBean> input){
+		this.items = input;
+	}
+}
